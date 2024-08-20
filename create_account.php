29ab452316d842476d2b36eca21f346cd155f668
Topeee <ramos.269841@cubao.sti.edu.ph>
2024-08-21 00:31:19 +0800

    aaaa

---
+++ create_account.php
@@ -0,0 +1,53 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root"; // XAMPP default username is 'root'
+$password = ""; // XAMPP default password is an empty string
+$database = "ossm_accounts";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $middle_name = $_POST['middlename'];
+    $suffix = $_POST['suffix-dropdown'];
+    $dob = $_POST['dob'];
+    $gender = $_POST['gender-dropdown'];
+    $mobile_number = $_POST['mn'];
+    $tel_number = $_POST['tn'];
+    $email = $_POST['email'];
+    $house_number = $_POST['house#'];
+    $street = $_POST['street'];
+    $barangay = $_POST['barangay-dropdown'];
+    $password = $_POST['password'];
+    
+    // Hash the password
+    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+    // Insert data into the database
+    $sql = "INSERT INTO users (first_name, last_name, middle_name, suffix, dob, gender, mobile_number, tel_number, email, house_number, street, barangay, password_hash)
+            VALUES ('$first_name', '$last_name', '$middle_name', '$suffix', '$dob', '$gender', '$mobile_number', '$tel_number', '$email', '$house_number', '$street', '$barangay', '$password_hash')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New account created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
+}
+?>

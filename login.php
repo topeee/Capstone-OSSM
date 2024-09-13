0923121e4eb27f5f7d8358d3e12d2ddecf91109b
Topeee <ramos.269841@cubao.sti.edu.ph>
2024-09-14 00:36:14 +0800

    TESTT

--- login.php
+++ login.php
@@ -37,38 +37,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-
-if (isset($_POST['submit_btn'])) {
-    
-    // Storing name in $name variable
-    $name = $_POST['name'];
-  
-    // Storing google recaptcha response
-    // in $recaptcha variable
-    $recaptcha = $_POST['g-recaptcha-response'];
-
-    // Put secret key here, which we get
-    // from google console
-    $secret_key = '6LdSsC8qAAAAAALv4qGXAlg-_krUe2lT2nsayFs8';
-
-    // Hitting request to the URL, Google will
-    // respond with success or error scenario
-    $url = 'https://www.google.com/recaptcha/api/siteverify?secret='
-          . $secret_key . '&response=' . $recaptcha;
-
-    // Making request to verify captcha
-    $response = file_get_contents($url);
-
-    // Response return by google is in
-    // JSON format, so we have to parse
-    // that json
-    $response = json_decode($response);
-
-    // Checking, if response is true or not
-    if ($response->success == true) {
-        echo '<script>alert("Google reCAPTACHA verified")</script>';
-    } else {
-        echo '<script>alert("Error in Google reCAPTACHA")</script>';
-    }
-}
-    ?>    // Removed closing PHP tag
+$conn->close();

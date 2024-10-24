81c632af7e46c31cd288ee2a18fce64dfcf70730
DHOME RAMOS <ramos.247754@cubao.sti.edu.ph>
2024-10-24 19:01:10 +0800

    Commit, pakisynch nalang

--- PWD-landing-page.php
+++ PWD-landing-page.php
@@ -21,7 +21,7 @@ include 'header.php';
 </head>
 
 <style>
-    body {
+body {
   background: linear-gradient(#00bfff, #87cefa);
   background-size: cover; 
   background-position: center;
@@ -165,28 +165,6 @@ html, body {
 
 </style>
 <body>
-    <nav class="navbar navbar-dark navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">
-                <img class="navbar-brand-logo" alt="Logo" src="logo.png" width="110" height="110">
-                <span class="brand-name">OSSM</span>
-            </a>
-            <div class="d-flex align-items-center ms-auto">
-                <span class="username">Hello, Username</span>
-                <div class="dropdown-center ms-3">
-                    <a class="btn btn-secondary dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img class="Hamburger-Icon" src="Burger icon.png" alt="Burger Icon" width="36" height="36">
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item" href="account_profile.html">Profile</a></li>
-                        <li><a class="dropdown-item" href="#">History Transaction</a></li>
-                        <li><a class="dropdown-item logout-item" href="login.html">Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
-
     <main>
         <div id="top-bar" class="top-bar-image d-flex justify-content-center align-items-center">
             <img src="C:\Users\rexce\Desktop\ossm\Capstone-OSSM\sagw.jpg" class="minibanner" alt="Social And General Welfare">

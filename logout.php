84153d18c6b44f0d0f73270f4a367f0a18aae8af
Christopher Ramos <ramos.269841@cubao.sti.edu.ph>
2024-10-25 20:52:44 +0800

    API GOOGLE

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?php
 session_start(); // Start the session
+require_once 'config.php'; 
 
 // Destroy the session and remove all session variables
 session_unset();
@@ -7,5 +8,19 @@ session_destroy();
 
 // Redirect to the login page
 header("Location: login.php");
-exit();
+// Include configuration file 
+ 
+// Remove token and user data from the session 
+unset($_SESSION['token']); 
+unset($_SESSION['userData']); 
+ 
+// Reset OAuth access token 
+$gClient->revokeToken(); 
+ 
+// Destroy entire session data 
+session_destroy(); 
+ 
+// Redirect to homepage 
+header("Location: login.php"); 
+exit(); 
 ?>

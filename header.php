62662a2cd2591ccb818ccd6809cf620b6149ffa3
Christopher Ramos <ramos.269841@cubao.sti.edu.ph>
2024-11-07 03:19:12 +0800

    account_profile.php

--- header.php
+++ header.php
@@ -57,7 +57,7 @@ if ($stmt = $conn->prepare($query)) {
         <div class="d-flex align-items-center ms-auto"><span class="username">Hello, <?php echo htmlspecialchars($first_name); ?></span>
             <div class="dropdown-center ms-3"><a class="btn btn-secondary dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img class="Hamburger-Icon" src="Burger icon.png" alt="Burger Icon" width="36" height="36"></a>
                 <ul class="dropdown-menu dropdown-menu-end">
-                    <li><a class="dropdown-item" href="account_profilee.php">Profile</a></li>
+                    <li><a class="dropdown-item" href="account_profile.php">Profile</a></li>
                     <li><a class="dropdown-item" href="trasaac_history.php">History Transaction</a></li>
                     <li><a class="dropdown-item logout-item" href="logout.php">Logout</a></li>
                 </ul>

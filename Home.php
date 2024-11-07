26d32e86eb1e923d4fcdae12fba1afbf2fdbb77a
Mico Skyler Sagat Manuel <micoskylermanuel@gmail.com>
2024-11-07 22:21:45 +0800

    testing again

--- Home.php
+++ Home.php
@@ -20,7 +20,6 @@ include 'header.php';
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
-
     <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap.min.js">
     <link rel="stylesheet" href="Footer.Clean.icons.css">

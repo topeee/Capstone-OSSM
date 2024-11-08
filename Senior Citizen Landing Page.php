1a27d3281ebf83ce3b6f3067f4d4d8d4a8634318
Mico Skyler Sagat Manuel <micoskylermanuel@gmail.com>
2024-11-09 00:24:37 +0800

    testing

--- Senior Citizen Landing Page.php
+++ Senior Citizen Landing Page.php
@@ -13,8 +13,8 @@ include 'header.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap.min.js">
+    <link rel="stylesheet" href="Footer.Clean.icons.css">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="footer.css">
     <link rel="icon" type="img/png" href="logo.png">
     <title>Senior Citizen Landing Page</title>
 </head>
@@ -164,6 +164,27 @@ html, body {
 
 </style>
 <body>
+    <nav class="navbar navbar-dark navbar-expand-lg" style="display:none">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img class="navbar-brand-logo" alt="Logo" src="logo.png" width="110" height="110">
+                <span class="brand-name">OSSM</span>
+            </a>
+            <div class="d-flex align-items-center ms-auto">
+                <span class="username">Hello, Username</span>
+                <div class="dropdown-center ms-3">
+                    <a class="btn btn-secondary dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img class="Hamburger-Icon" src="Burger icon.png" alt="Burger Icon" width="36" height="36">
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="account_profile.html">Profile</a></li>
+                        <li><a class="dropdown-item" href="#">History Transaction</a></li>
+                        <li><a class="dropdown-item logout-item" href="login.html">Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
 
     <main>
         <div id="top-bar" class="top-bar-image d-flex justify-content-center align-items-center">
@@ -373,7 +394,7 @@ html, body {
                         </div>
 
                     <div class="wrap">
-                        <a href="Senior Citizen Application Form.php">
+                        <a href="Senior Citizen Application Form.html">
                             <button class="button">Apply Now?</button>
                         </a>
                     </div>
@@ -428,8 +449,7 @@ html, body {
                 </div>
             </div>
         </div>            
-    </footer>    
-
+    </footer> 
 
     <script>
         document.addEventListener("DOMContentLoaded", function() {

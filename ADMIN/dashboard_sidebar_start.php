aebc55b5747cadb5ae480a1e1298629a49f3d97e
Mico Skyler Sagat Manuel <micoskylermanuel@gmail.com>
2024-11-10 01:42:39 +0800

    connected

--- ADMIN/dashboard_sidebar_start.php
+++ ADMIN/dashboard_sidebar_start.php
@@ -34,7 +34,7 @@
         <ul class="navbar-nav bg-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admn_dashboard.php">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="adminDashboard.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     
                 </div>
